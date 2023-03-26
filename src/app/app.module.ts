425bf48173698264609e7fbed3964894fa2925d3
Hieu Bui Van <hieubv1306@gmail.com>
2023-03-26 21:42:55 +0700

    #1. What's the component? and life cycle hooks. #2. Communication data between two component; #3. Text interpolation; #4. Data binding;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world.component';
+import { ChildComComponent } from './1-comunication-components/child-com/child-com.component';
+import { ParentComComponent } from './1-comunication-components/parent-com/parent-com.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent,
+    ChildComComponent,
+    ParentComComponent,
   ],
   imports: [
     BrowserModule,
